7283920ca4907cb63b9097d4ea7a017101764732
dali2579 <ma.benamor@gmail.com>
2021-01-29 17:13:05 +0100

    update the ibmcloudant couch db address

--- angular-PouchDB-Attchement/src/app/data.service.ts
+++ angular-PouchDB-Attchement/src/app/data.service.ts
@@ -10,7 +10,8 @@ import PouchDB from "pouchdb";
 export class DataService {
     db: any;
     listener:EventEmitter<any>=new EventEmitter();
-    remoteCouch:string = 'http://admin:azerty123@127.0.0.1:5984/angularpouchdbattchement';
+    remoteCouch:string = "https://apikey-v2-xpr4pf78ynije6aecc2fwvtt0ry6j09y3dsstabtk76:2b89ced1c7a482b00660ecb52cdcbce5@eb7da67c-420f-4c17-b8af-e4880f8703b3-bluemix.cloudantnosqldb.appdomain.cloud/angularpouchdbattchement";
+    //'http://admin:azerty123@127.0.0.1:5984/angularpouchdbattchement';
     constructor() {
         this.db=new PouchDB("angularpouchdbattchement");    
     }
